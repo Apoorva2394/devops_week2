733ee9318c659f5f6ff83c27fc60aa20248e013a
Apoorva gupta <apoorvagupta@Apoorvas-MacBook-Air-2.local>
2025-12-12 15:32:51 +0530

    files added

--- java1.java
+++ java1.java
@@ -1,3 +1,10 @@
-public class java1 {
-    
+class java1{
+    public static void main(string[] args){
+        System.out.println("hello from ubuntu!");
+        int n=8;
+        System.out.println("Table of 8:");
+        for (int i=0;i<=10;i++){
+            System.out.println(n +"*"+ i +"="+ (n*i));
+        }
+    }
 }
